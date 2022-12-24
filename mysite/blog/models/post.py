2f5f68e0ae5d89da8243f74770a6f6cf75a4b74b
JonasSousa <jonassousa.dev@gmail.com>
2022-12-24 17:07:18 +0000

    add comments

--- mysite/blog/models/post.py
+++ mysite/blog/models/post.py
@@ -25,3 +25,17 @@ class Post(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['created_on']  #ascending
+
+    def __str__(self):
+        return self.content
+
+
+
